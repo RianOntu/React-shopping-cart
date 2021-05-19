3fcea25b93eeeca136e159d9cddd01e8f54c8251
RianOntu <rianontu13@gmail.com>
2021-05-19 17:33:17 +0600

    some css changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ const Product=({product,onAdd})=>{
        
             
             <div className="card" style={{alignItems:"center",height:"350px"}}>
-  <img src={product.image} alt="Avatar" style={{width:"100px",height:"92px",marginLeft:"32%"}}/>
+  <img src={product.image} alt="Avatar" style={{width:"100px",height:"100px",marginLeft:"32%"}}/>
   <div style={{justifyContent:"center"}}>
     <h4 style={{textAlign:"center",marginTop:"10%"}}><b>{product.title}</b></h4> 
     <p style={{textAlign:"center"}}>BDT {product.price}</p> 
